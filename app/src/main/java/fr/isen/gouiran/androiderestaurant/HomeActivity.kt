ccfedb2157521e5c1c02d16ca7b1f4917fba4702
Hugo30128 <104615072+Hugo30128@users.noreply.github.com>
2023-01-16 14:10:28 +0100

    button

--- app/src/main/java/fr/isen/gouiran/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/gouiran/androiderestaurant/HomeActivity.kt
@@ -1,19 +1,34 @@
 package fr.isen.gouiran.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.google.android.material.snackbar.Snackbar
-import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val action = findViewById<Button>(R.id.buttonEntree)
-        action.setOnClickListener{
-           //Toast.makeText( context: this; text = "Hello ISEN M1"; Toast.LENGTH_SHORT).show()
-            Snackbar.make(it, "Hello ISEN M1", Snackbar.LENGTH_LONG).show()
+        val entree = findViewById<Button>(R.id.buttonEntree)
+        val plat = findViewById<Button>(R.id.buttonPlats)
+        val dessert = findViewById<Button>(R.id.buttonDessert)
+
+        entree.setOnClickListener {
+            val button = Intent(applicationContext, CategoryActivity::class.java)
+            intent.putExtra("ActivityName", "Entree")
+            startActivity(intent);
         }
+
+        plat.setOnClickListener {
+            val button = Intent(applicationContext, CategoryActivity::class.java)
+            intent.putExtra("ActivityName", "Plats")
+            startActivity(intent)
+        }
+        dessert.setOnClickListener {
+            val button = Intent(applicationContext, CategoryActivity::class.java)
+            intent.putExtra("ActivityName", "Dessert")
+            startActivity(intent)
+        }
+
     }
 }
