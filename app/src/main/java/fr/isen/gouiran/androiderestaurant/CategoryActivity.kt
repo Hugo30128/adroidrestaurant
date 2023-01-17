4298cddbcf495ed67ae29ced2c700f7580876042
hgoui <hgouiran@gmail.com>
2023-01-17 16:57:54 +0100

    JSON

--- app/src/main/java/fr/isen/gouiran/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/gouiran/androiderestaurant/CategoryActivity.kt
@@ -2,9 +2,14 @@ package fr.isen.gouiran.androiderestaurant
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.toolbox.Volley
 import fr.isen.gouiran.androiderestaurant.databinding.ActivityCategoryBinding
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import org.json.JSONObject
 
 class CategoryActivity : AppCompatActivity() {
 
@@ -19,16 +24,27 @@ class CategoryActivity : AppCompatActivity() {
         actionBar?.title = category
 
         val dishes = resources.getStringArray(R.array.plats).toList() as ArrayList<String>
+
         binding.categoryList.layoutManager = LinearLayoutManager(this)
-        binding.categoryList.adapter = CustomAdapter(dishes){
+        binding.categoryList.adapter = CustomAdapter(arrayListOf()) {
             val intent = Intent(this, DetailActivity::class.java)
             startActivity(intent)
         }
-
-
-
-        }
-
+        loadDishesFromAPI()
+    }
+    private fun loadDishesFromAPI(){
+       Volley.newRequestQueue(this)
+        val url = "http://test.api.catering.bluecodegames.com/menu"
+        val jsonObject = JSONObject()
+        jsonObject.put("id_shop", "1")
+        val jsonRequest = JsonObjectRequest(Request.Method.POST, url, jsonObject, {
+                                                                                  Log.w("CategoryActivity", "response : $it")
+                                                                                  },{
+                                                                                  Log.w("CategoryActivity", "erreur: $it")
+                                                                                  }
+        )
+        Volley.newRequestQueue(this).add(jsonRequest)
+    }
 }
 
 
